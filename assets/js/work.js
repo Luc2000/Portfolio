45ea61f5322f804f88248f2fece3e7d1e5654929
Lucas Souza <lucas.annunziato@aluno.ifsp.edu.br>
2021-10-14 14:24:57 -0300

    first commit

---
+++ assets/js/work.js
@@ -0,0 +1,179 @@
+async function grabImages(name, id){
+    let result;
+
+    try {
+        result = await $.ajax({
+            type:"get",
+            url:"https://raw.githubusercontent.com/Luc2000/"+name+"/master/background/background.jpg",
+        });
+
+        return result;
+    } catch (error) {
+
+    }
+}
+
+function insertImages(data, id, name){
+    if(data){
+        $('#repo_'+id+' .repo-background').css('background-image', "url(https://raw.githubusercontent.com/Luc2000/"+name+"/master/background/background.jpg)");
+    }
+}
+
+
+function competencias(){
+    fetch("/portfolio/assets/js/abilities.json")
+        .then(response => response.json())
+        .then($('.projects-list').html(`<ul id="competencias_gerais" class="d-flex flex-wrap justify-content-between"></ul>`))
+        .then(data => 
+            data.forEach(competencia => {
+                $('#competencias_gerais').append(`
+                    <li class="competencia card d-flex flex-wrap position-relative mb-35 ">
+                        <div class="d-block header-competencia text-center">
+                            <div class="stats mb-3">
+                                ${competencia.icon}
+                            </div>
+                            <b>${competencia.name}</b>
+                        </div>
+                        <p class="mt-35">${competencia.description}</p>
+                    </li>
+                `);
+            })
+        ).catch(function() {
+            console.log("Erro ao puxar JSON de competências");
+        });
+}
+
+
+// Inserir em languages dps: para detalharsua formação front back etc 
+// <div class="more_about d-flex col-12 align-items-center">
+//      <a href="/front-end" target="_blank"><i class="fas fa-question-circle"></i><b> Mais detalhes</b></a>                                         
+// </div> 
+function languages(){
+    fetch("/portfolio/assets/js/languages.json")
+        .then(response => response.json())
+        .then($('.projects-list').html(`<ul class="d-flex flex-wrap justify-content-between" id="languages"></ul>`))
+        .then(data => 
+            data.forEach(materia => {
+                var status = '';
+                switch (materia.status) {
+                    case 0:
+                        status = 'estudando'
+                        break;
+                    case 1:
+                        status = 'experiente'
+                        break;
+                    case 2:
+                        status = 'estudado'
+                        break;
+                    default:
+                        break;
+                }
+                $('#languages').append(`
+                    <li class="d-flex flex-wrap card position-relative mb-35">
+                        <div class="tag ${status} position-absolute">
+                            <span class="text-capitalize">${status}</span>
+                        </div>
+                        <div class="content-card">
+                            <img src=${materia.iconUrl} alt="${materia.name}" class="logo-materia">
+                            <h4 class="mt-3"><b>${materia.name}</b></h4>
+
+                            <div class="grades-content row mt-20">
+                                <div class="ability d-flex col-12" tabindex="0" data-toggle="tooltip" title="Habilidade" data-placement="top">
+                                    <div class="d-flex progress">
+                                        <i class="fas fa-fire"></i>                                              
+                                        <div class="progress-bar">
+                                            <div class="progress-done" data-done="${materia.ability}" style="width:${materia.ability}%"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="ability d-flex col-12" tabindex="0" data-toggle="tooltip" title="Experiência" data-placement="top">
+                                    <div class="d-flex progress">
+                                        <i class="fas fa-hourglass-half"></i>                                                
+                                        <div class="progress-bar">
+                                            <div class="progress-done" data-done="${materia.experience}" style="width:${materia.experience}%"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="ability d-flex col-12" tabindex="0" data-toggle="tooltip" title="Boas práticas" data-placement="top">
+                                    <div class="d-flex progress">
+                                        <i class="fas fa-code"></i>                                                
+                                        <div class="progress-bar">
+                                            <div class="progress-done" data-done="${materia.code}" style="width:${materia.code}%"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="ability d-flex col-12">
+                                    <b>Projetos Feitos: ${materia.projects}</b>                                              
+                                </div>
+                               
+                            </div>
+                        </div>
+                    </li>
+                `);
+            })
+        ).catch(function() {
+            console.log("Erro ao puxar API");
+        });
+}
+
+function projects(){
+    $.ajax({
+        type:"get",
+        url:"https://api.github.com/users/Luc2000/repos",
+        dataType:"json"
+    }).done(function(data){
+        $('.projects-list').html(`<ul id="github_repos" class="d-flex flex-wrap justify-content-between"></ul>`);
+
+        data.forEach(repo => {
+            var background_img = "assets/img/github.png";
+            var date = new Intl.DateTimeFormat('pt-BR').format(new Date(repo.created_at));
+
+            $('#github_repos').append(`
+                <li class="repo d-flex flex-wrap position-relative mb-35" id="repo_${repo.id}">
+                    <div class="repo-background" style="background-image: url(${background_img})">
+                    </div>
+                    <div class="repo-body d-block flex-wrap">
+                        <p class="my-2"><i class="far fa-calendar-alt mr-2 "></i>${date}</p>
+                        <p class="my-3"><i class="fas fa-info mx-1"></i>`+(repo.description ? repo.description : 'Projeto feito no Github, sem descrição.')+`</p>
+                        `+(repo.homepage ? '<p class="my-2"><i class="fas fa-globe mr-1"></i> <a href="'+repo.homepage+'" target="_blank" rel="noopener">'+repo.homepage+'</a></p>' : '')+`
+                    </div>
+                    <div class="ver_git">
+                        <p><a href="${repo.html_url}" target="_blank" rel="noopener">Ver no Github <i class="fab fa-github ml-2"></i></i></a></p>
+                    </div>
+                </li>
+            `);
+            grabImages(repo.name,repo.id).then((data) => insertImages(data,repo.id, repo.name));
+        });             
+    }).fail(function(data){
+        alert('Erro ao exibir, tente novamente mais tarde');
+    });
+}
+
+function courses(){
+    fetch("/portfolio/assets/js/courses.json")
+        .then(response => response.json())
+        .then($('.projects-list').html(`<ul id="cursos" class="d-flex flex-wrap justify-content-between"></ul>`))
+        .then(data => 
+            data.forEach(curso => {
+                $('#cursos').append(`
+                    <li class="curso card d-flex flex-wrap position-relative mb-35 ">
+                        <div class="header-curso d-flex mb-2 w-100 align-items-center">
+                            <img src=${curso.iconImg} alt="${curso.course}" class="mr-3 mb-3"/>
+                            <b>${curso.course}</b>
+                        </div>
+                        <div class="body d-block">
+                            <p><i class="far fa-calendar-alt"></i> ${curso.date}</p>
+                            <p class="my-3"><i class="fas fa-map-marker-alt"></i> <a href="${curso.where}" target="_blank" rel="noopener">${curso.where}</a></p>
+                            <p><i class="fas fa-info"></i> ${curso.about}</p>
+                        </div>
+                    </li>
+                `);
+            })
+        ).catch(function() {
+            console.log("Erro ao puxar JSON de competências");
+        });
+}
+
+$(function(){
+    competencias();
+});
---
+++ inc/header.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="pt-bt">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="discrption" content="Portfólio Desenvolvedor Fullstack" />
+    <meta name="keyword" content="freelancer, fullstack, desenvolvedor brasileiro, dev, contratar backend" />
+
+    <!--  Title -->
+    <title>Dev Full Stack - Annunziato. Lucas</title>
+
+    <!-- Font Google -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700,800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,600,700&display=swap" rel="stylesheet" />
+
+    <!-- JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="assets/img/favicon.ico" type="image/x-icon" />
+
+    <!-- custom styles -->
+    <link href="assets/css/plugins.css" rel="stylesheet" />
+    <link href="assets/css/style.css" rel="stylesheet" />
+</head>
+
+<body class="hamburger-menu dsn-effect-scroll dsn-ajax" data-dsn-mousemove="true">
+
+    <div class="preloader">
+        <div class="preloader-after"></div>
+        <div class="preloader-before"></div>
+        <div class="preloader-block">
+            <div class="title">LucDev</div>
+            <div class="percent">0</div>
+            <div class="loading">carregando...</div>
+        </div>
+        <div class="preloader-bar">
+            <div class="preloader-progress"></div>
+        </div>
+    </div>
+
+    <!-- Nav Bar -->
+    <div class="dsn-nav-bar">
+        <div class="site-header">
+            <div class="extend-container">
+                <div class="inner-header">
+                    <div class="main-logo">
+                        <a href="index.php">
+                            <div class="animate-svg">
+                                <svg x="0px" y="0px" viewBox="0 0 466 463" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M277.266 108.838C277.148 95.5451 272.455 90.8765 269 81C281.371 93.5972 290.16 100.684 303.971 112.635V340.404L339.5 307.5C339.578 305 339.578 305.5 339.578 301.81V267.012V247.5L365.012 273V289.156V314.5L269 410C274.341 401.011 276.135 395.321 277.266 384.06V108.838Z" 
+                                        fill="none"/>
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M174.034 280.167L207.552 312.772C201.855 303.541 199.993 297.8 198.862 286.612V230.643H234.862V316.422C234.862 328 234.862 325.5 234.862 333L269 373C262.657 362.038 260.83 355.458 259.689 343.19V73.6844C261.878 65.0265 263.908 60.5585 268.379 53L174.034 145.213C174.034 154.5 174.034 158 174.034 167.981V258.019C174.034 266.5 174.034 270.5 174.034 280.167ZM234.241 206.308H197.621V155.814L233.621 122.354L234.241 206.308Z"
+                                        fill="none"/>
+                                    <path d="M269.5 446.5L269 419L438 250L269 81V53L466 250L269.5 446.5Z" 
+                                        fill="none"/>
+                                    <path d="M268.5 409.5L269 382L100 213L269 44V16L72 213L268.5 409.5Z" 
+                                        fill="none"/>
+
+                                    <circle cx="274" cy="23" r="19" fill="#e1e1e1" stroke-width="8"/>
+                                    <circle cx="23" cy="213" r="19" fill="#e1e1e1" stroke-width="8"/>
+                                    <circle cx="265" cy="440" r="19" fill="#3bc0dd" stroke-width="8"/>
+                                    <rect x="38" y="203" width="63" height="20" fill="none"/>
+                                </svg>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <nav class=" accent-menu main-navigation">
+                    <ul class="extend-container">
+                        <li class="custom-drop-down">
+                            <a href="index.php">Competências e Trabalhos</a>
+                        </li>
+                        <li>
+                            <a href="sobre.php">Sobre mim</a>
+                        </li>
+                        <li><a href="contato.php">Contato</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <div class="header-top header-top-hamburger">
+            <div class="header-container">
+                <div class="logo main-logo">
+                    <a href="index.php">
+                        <div class="animate-svg">
+                            <svg x="0px" y="0px" viewBox="0 0 466 463" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            
+                                    <rect x="38" y="203" width="63" height="20" fill="#fff"/>    
+                                    <!-- L -->
+                                    <path class="lucas" d="M277.266 108.838C277.148 95.5451 272.455 90.8765 269 81C281.371 93.5972 290.16 100.684 303.971 112.635V340.404L339.5 307.5C339.578 305 339.578 305.5 339.578 301.81V267.012V247.5L365.012 273V289.156V314.5L269 410C274.341 401.011 276.135 395.321 277.266 384.06V108.838Z" 
+                                        fill="none"/>   
+                                    <!-- A -->
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M174.034 280.167L207.552 312.772C201.855 303.541 199.993 297.8 198.862 286.612V230.643H234.862V316.422C234.862 328 234.862 325.5 234.862 333L269 373C262.657 362.038 260.83 355.458 259.689 343.19V73.6844C261.878 65.0265 263.908 60.5585 268.379 53L174.034 145.213C174.034 154.5 174.034 158 174.034 167.981V258.019C174.034 266.5 174.034 270.5 174.034 280.167ZM234.241 206.308H197.621V155.814L233.621 122.354L234.241 206.308Z"
+                                        fill="#fff"/>
+                                    <!-- LATERAL DIR -->
+                                    <path d="M269.5 446.5L269 419L438 250L269 81V53L466 250L269.5 446.5Z" 
+                                        fill="#fff"/>
+                                    <!-- LATERAL ESQ -->
+                                    <path d="M268.5 409.5L269 382L100 213L269 44V16L72 213L268.5 409.5Z" 
+                                        fill="#fff"/>
+                                        
+
+                                    <circle cx="274" cy="23" r="19" fill="#fff" stroke-width="8"/>
+                                    <circle cx="23" cy="213" r="19" fill="#fff" stroke-width="8"/>
+                                    <circle cx="265" cy="440" r="19" fill="#fff" stroke-width="8"/>
+                                    <!-- Retangulo -->
+                                
+                            </svg>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="menu-icon" data-dsn="parallax" data-dsn-move="5">
+                    <div class="icon-m">
+                        <i class="menu-icon-close fas fa-times"></i>
+                        <span class="menu-icon__line menu-icon__line-left"></span>
+                        <span class="menu-icon__line"></span>
+                        <span class="menu-icon__line menu-icon__line-right"></span>
+                    </div>
+
+                    <div class="text-menu">
+                        <div class="text-button">Menu</div>
+                        <div class="text-open">Abrir</div>
+                        <div class="text-close">Fechar</div>
+                    </div>
+                </div>
+
+                <div class="nav">
+                    <div class="inner">
+                        <div class="nav__content">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="nav-content">
+                    <div class="inner-content">
+                        <address class="v-middle">
+                            <span>Desenvolvedor Fullstack</span>
+                            <span><a href="mailto:lucassouzaannunziato@gmail.com">Enviar um e-mail</a></span>
+                        </address>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Nav Bar -->
